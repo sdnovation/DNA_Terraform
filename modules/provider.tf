41e150570e88c2c7c80921388ef54a9661658148
sdnovation <155182829+sdnovation@users.noreply.github.com>
2024-01-08 17:28:58 +0100

    Delete modules/provider.tf

--- modules/provider.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    dnacenter = {
-      source = "cisco-en-programmability/dnacenter"      
-      #version = "1.1.31-beta"
-      version = "1.1.21"    
-    }
-  }
-}
